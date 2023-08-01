9f3896f36f0a32b3470217d4cb3048e5d86f3017
kalaniJ94 <kalaniJ94@gmail.com>
2023-08-01 13:41:01 -0600

    Block colors updating properly

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+$(document).ready(function() {
 function updateTime(){
   var currentDayEL = document.getElementById("currentDay");
   var today = dayjs();
@@ -10,34 +12,7 @@ function updateTime(){
 }
 setInterval(updateTime, 1000);
 
-$(function () {
-function saveText(hourId) {
-  var inputText = document.querySelector("inputText"); // Get the text from the corresponding textarea
-
-  localStorage.setItem(hourId, inputText); // Save the text in localStorage with the hourId as the key
-}
-
-function loadSavedText(){
-  for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    var hourId = i.value
-    
-  }
-}
- document.addEventListener("DOMContentLoaded", function(){
-  loadSavedText();
- })
-
- var saveButtons = document.querySelectorAll(".saveBtn");
- saveButtons.forEach(function (button) {
-     button.addEventListener("click", function () {
-         var hourId = this.parentNode.id; // Get the parent div's id, which corresponds to the hour (e.g., "9AM")
-         saveText(hourId);
-        });
-      });
   
-
-
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -45,19 +20,65 @@ function loadSavedText(){
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-
-
+  function saveText(hourId) {
+    var inputText = document.querySelector("inputText"); // Get the text from the corresponding textarea
+  
+    localStorage.setItem(hourId, inputText); // Save the text in localStorage with the hourId as the key
+  }
+  
+  
+  var saveButtons = document.querySelectorAll(".saveBtn");
+  saveButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      var hourId = this.parentNode.id; // Get the parent div's id, which corresponds to the hour (e.g., "9AM")
+      saveText(hourId);
+    });
+  });
+  
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  function blockColor(){
+    var timeBlock = document.querySelectorAll(" .time-block");
+    var currentHour = parseInt(dayjs().format('H'));
+    
+    timeBlock.forEach(function (timeBlock){
+      var hour = parseInt(timeBlock.getAttribute("data-typeID"));
+      if(hour < currentHour){
+        timeBlock.classList.add('past');
+            timeBlock.classList.remove('present', 'future');
+        } else if (hour === currentHour) {
+            timeBlock.classList.add('present');
+            timeBlock.classList.remove('past', 'future');
+        } else {
+            timeBlock.classList.add('future');
+            timeBlock.classList.remove('past', 'present');
+      }
+    }
+    )
+    console.log("blockColor function called");
+    console.log("Current Hour:", currentHour);
+  };
+  
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  // function loadSavedText(){
+  //   for (let i = 0; i < array.length; i++) {
+  //     const element = array[i];
+  //     var hourId = i.value
+      
+  //   }
+  // }
   //
   // TODO: Add code to display the current date in the header of the page.
-})
+  //  loadSavedText();
+   blockColor(); 
+   updateTime();
+  });
+      
 
-updateTime();
